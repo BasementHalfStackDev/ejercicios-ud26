ee7e545624aa5960ee0cb8d78db30768be4aea00
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-18 20:38:01 +0200

    E4 Services done

--- JPallas_TA26_E4/src/main/java/E4/service/EquipoService.java
+++ JPallas_TA26_E4/src/main/java/E4/service/EquipoService.java
@@ -3,6 +3,40 @@
  */
 package E4.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import E4.dao.EquipoDAO;
+import E4.dto.Equipo;
+
+@Service // Assign Service
 public class EquipoService {
+
+	// Load DAO
+	@Autowired
+	EquipoDAO equipoDAO;
+
+	// CRUD functions
+	public List<Equipo> listEquipos() {
+		return equipoDAO.findAll();
+	}
+
+	public Equipo saveEquipo(Equipo equipo) {
+		return equipoDAO.save(equipo);
+	}
+
+	public Equipo equipoById(String id) {
+		return equipoDAO.findById(id).get();
+	}
+
+	public Equipo updateEquipo(Equipo equipo) {
+		return equipoDAO.save(equipo);
+	}
+
+	public void deleteEquipo(String id) {
+		equipoDAO.deleteById(id);
+	}
 
 }
--- JPallas_TA26_E4/src/main/java/E4/service/ReservaService.java
+++ JPallas_TA26_E4/src/main/java/E4/service/ReservaService.java
@@ -3,6 +3,40 @@
  */
 package E4.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import E4.dao.ReservaDAO;
+import E4.dto.Reserva;
+
+@Service // Assign Service
 public class ReservaService {
+
+	// Load DAO
+	@Autowired
+	ReservaDAO reservaDAO;
+
+	// CRUD functions
+	public List<Reserva> listReservas() {
+		return reservaDAO.findAll();
+	}
+
+	public Reserva saveReserva(Reserva reserva) {
+		return reservaDAO.save(reserva);
+	}
+
+	public Reserva reservaById(Long id) {
+		return reservaDAO.findById(id).get();
+	}
+
+	public Reserva updateReserva(Reserva reserva) {
+		return reservaDAO.save(reserva);
+	}
+
+	public void deleteReserva(Long id) {
+		reservaDAO.deleteById(id);
+	}
 
 }
