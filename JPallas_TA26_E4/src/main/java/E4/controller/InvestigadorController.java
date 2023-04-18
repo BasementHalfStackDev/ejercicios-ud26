ef01be708478f9ef191d7a7500aa97784fe1eb71
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-18 20:46:04 +0200

    E4 finished

---
+++ JPallas_TA26_E4/src/main/java/E4/controller/InvestigadorController.java
@@ -0,0 +1,63 @@
+/**
+ * @author BasementHalfStackDev/Josep Maria Pallas Batalla
+ */
+package E4.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import E4.dto.Investigador;
+import E4.service.InvestigadorService;
+
+@RestController // Rest controller
+@RequestMapping("/api")
+public class InvestigadorController {
+
+	// Implement service
+	@Autowired
+	InvestigadorService investigadorService;
+
+	@GetMapping("/investigadores")
+	public List<Investigador> listInvestigadores() {
+		return investigadorService.listInvestigadores();
+	}
+
+	@GetMapping("/investigadores/{dni}")
+	public Investigador investigadorById(@PathVariable(name = "dni") String dni) {
+		Investigador investigadorxID = new Investigador();
+
+		investigadorxID = investigadorService.InvestigadorById(dni);
+
+		return investigadorxID;
+	}
+
+	@PostMapping("/investigadores")
+	public Investigador saveInvestigador(@RequestBody Investigador investigador) {
+		return investigadorService.saveInvestigador(investigador);
+	}
+
+	@PutMapping("/investigadores/{dni}")
+	public Investigador updateInvestigador(@PathVariable(name = "dni") String dni,
+			@RequestBody Investigador investigador) {
+		Investigador selectedInvestigador = new Investigador(dni, investigador.getNombre(), investigador.getFacultad());
+		Investigador updatedInvestigador = new Investigador();
+
+		updatedInvestigador = investigadorService.updateInvestigador(selectedInvestigador);
+		return updatedInvestigador;
+	}
+
+	@DeleteMapping("/investigadores/{dni}")
+	public void deleteInvestigador(@PathVariable(name = "dni") String dni) {
+		investigadorService.deleteInvestigador(dni);
+	}
+
+}
--- JPallas_TA26_E4/src/main/java/E4/dto/Reserva.java
+++ JPallas_TA26_E4/src/main/java/E4/dto/Reserva.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "reservas")
@@ -30,8 +32,10 @@ public class Reserva {
 	@JoinColumn(name = "investigador")
 	private Investigador investigador;
 
+	@Temporal(TemporalType.TIMESTAMP)
 	private LocalDateTime comienzo;
 
+	@Temporal(TemporalType.TIMESTAMP)
 	private LocalDateTime fin;
 
 	// Constructors
