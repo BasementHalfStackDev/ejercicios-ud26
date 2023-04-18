9019dd5db8e76547dee4643d8a70365e78db48b9
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-18 18:22:50 +0200

    E3 first Commit

---
+++ JPallas_TA26_E3/src/main/java/E3/JPallasTa26E3Application.java
@@ -0,0 +1,13 @@
+package E3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPallasTa26E3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPallasTa26E3Application.class, args);
+	}
+
+}
---
+++ JPallas_TA26_E3/src/main/java/E3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package E3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JPallasTa26E3Application.class);
+	}
+
+}
